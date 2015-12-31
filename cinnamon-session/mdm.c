8f83de1beaa288c853ca2304b0178ca7efa9599b
monsta <monsta@inbox.ru>
2015-12-31 10:38:01 +0300

    mdm: fix logic error

--- cinnamon-session/mdm.c
+++ cinnamon-session/mdm.c
@@ -286,7 +286,7 @@ mdm_init_protocol_connection (MdmProtocolData *data)
 
         response = mdm_send_protocol_msg (data, MDM_PROTOCOL_MSG_VERSION);
 
-        if (!response || strncmp (response, "MDM ", strlen ("MDM ") != 0)) {
+        if (!response || strncmp (response, "MDM ", strlen ("MDM ")) != 0) {
                 g_free (response);
 
                 g_warning ("Failed to get protocol version from MDM");
